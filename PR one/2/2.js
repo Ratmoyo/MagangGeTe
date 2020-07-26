5cec6646565764e5381771d40091728b757f2c1c
ratmoyo <ratmoyo2002@gmail.com>
2020-07-26 17:25:28 +0700

    2 modul oke

---
+++ PR one/2/2.js
@@ -0,0 +1,4 @@
+var emoji = require('node-emoji')
+
+console.log(emoji.get('heart'))
+console.log(emoji.random())
---
+++ pertemuan one/satu.js
@@ -0,0 +1,18 @@
+/*console.log("ini keluar teks");
+
+console.log(7 * 7);*/
+
+function tambah(angkaprtama, angkakedua) {
+    let hasil;
+
+    hasil = angkaprtama + angkakedua;
+    return hasil;
+}
+
+
+console.log(tambah(31, 55));
+
+/*const satu = 1;
+var helloworld = "hello world";
+helloworld = "ini hello world baru";
+console.log(helloworld);*/
