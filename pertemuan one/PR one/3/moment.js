dad5000fa344287c3f0af2b42948a8d8f4a45472
ratmoyo <ratmoyo2002@gmail.com>
2020-08-01 10:20:03 +0700

    pr done 3

---
+++ pertemuan one/PR one/3/moment.js
@@ -0,0 +1,9 @@
+var moment = require('moment');
+
+console.log(moment().format('LLL'))
+
+/* moment().format('MMMM Do YYYY, h:mm:ss a'); // August 1st 2020, 10:14:02 am
+moment().format('dddd');                    // Saturday
+moment().format("MMM Do YY");               // Aug 1st 20
+moment().format('YYYY [escaped] YYYY');     // 2020 escaped 2020
+moment().format();                          */
